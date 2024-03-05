c11d81de97c45916f8a4e39545c2ecce66d19472
Prem Kumar <95973265+IMKnightRider@users.noreply.github.com>
2024-03-05 14:25:45 +0000

    navbar updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,58 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-const Navbar = () => {
-  return (
-    <nav>
-        <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/blogs">Blogs</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-        </ul>
-    </nav>
-  )
+import { Link } from "react-router-dom"
+
+
+const navigation = [
+  { name: 'Home', href: '/', current: true },
+  { name: 'Blogs', href: '/blogs', current: false },
+  { name: 'Projects', href: '#', current: false },
+  { name: 'Calendar', href: '#', current: false },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
 }
 
-export default Navbar
+export default function Navbar() {
+  return (
+        <>
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm">
+                {/* Mobile menu button*/}
+                <button
+                  type="button"
+                  className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                  aria-controls="mobile-menu"
+                  aria-expanded="false"/>
+                
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
+                 
+                </div>
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <Link
+                        key={item.name}
+                        to={item.href}
+                        className={classNames(
+                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          'rounded-md px-3 py-2 text-sm font-medium'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+
+          
+        </>
+  )
+}
--- src/Pages/Blogs.jsx
+++ src/Pages/Blogs.jsx
@@ -6,7 +6,7 @@ const Blogs = () => {
   const { blog } = useParams()
   return (
     <>
-    <div>Blogs </div>
+    <div className='text-3xl font-bold underline'>Blogs </div>
     <Link to="/blogs/blog1">Blog 1</Link> <br/>
     <Link to="/blogs/blog2">Blog 2</Link> <br />
     <Link to="/blogs/blog3">Blog 3</Link> <br/>
